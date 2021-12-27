021ddce1c3173be95fbb96a8d76f93a76bb70c6a
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-12-27 20:21:58 +0100

    feat: Fetching and rendering posts

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,10 +1,13 @@
-import { PostCreate } from './components';
+import { PostCreate, PostList } from './components';
 
 export default function App() {
   return (
     <div className="container">
       <h1>Create Post</h1>
       <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
 }
---
+++ client-app/src/components/PostList.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const PostList = () => {
+  const [posts, setPosts] = useState({});
+
+  const fetchPosts = async () => {
+    const res = await axios.get('http://localhost:4000/posts');
+    setPosts(res.data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const renderedPosts = Object.values(posts).map((post) => {
+    return (
+      <div
+        className="card"
+        key={post.id}
+        style={{ width: '30%', marginBottom: '20px' }}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderedPosts}
+    </div>
+  );
+};
