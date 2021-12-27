b2ced2ae28de016ac453ab5ae4099dc2db09da37
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-12-27 19:20:52 +0100

    feat: implementing a comments service

---
+++ comments/index.js
@@ -0,0 +1,24 @@
+const expres = require('express');
+const { randomBytes } = require('crypto');
+
+const app = expres();
+app.use(expres.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content });
+  commentsByPostId[req.params.id] = comments;
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
