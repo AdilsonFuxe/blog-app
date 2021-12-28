7f7317785a1762af003f4a74f6c2a70de5527530
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-12-28 20:40:13 +0100

    feat: updating comment content

--- comments/index.js
+++ comments/index.js
@@ -34,8 +34,27 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Received event: ', req.body.type);
+
+  const { type, data } = data;
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => comment.id === id);
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        postId,
+        id,
+        status,
+        content,
+      },
+    });
+  }
   res.send({});
 });
 
