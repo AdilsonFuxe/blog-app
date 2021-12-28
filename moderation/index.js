4ae5363ed5581a9e33918576cb8a03d925dc3eda
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-12-28 20:27:47 +0100

    feat: handling moderation

--- moderation/index.js
+++ moderation/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/events', (req, res) => {});
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: { ...data, status },
+    });
+  }
+  res.send({});
+});
 
 app.listen(4003, () => {
   console.log('Listening on 4003');
