da3d0b9c1c6a8abbb2f841ac0e6dba56bb79d0c6
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-12-28 03:07:40 +0100

    feat: emitting post creation events

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(cors());
@@ -12,13 +13,22 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
   posts[id] = {
     id,
     title,
   };
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: {
+      id,
+      title,
+    },
+  });
+
   res.status(201).send(posts[id]);
 });
 
