2ac4f1338cd39d7d1f2b218fd07b2eeb8cccd387
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-12-28 03:13:39 +0100

    feat: emitting comments creation events

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const expres = require('express');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = expres();
 app.use(cors());
@@ -12,12 +13,22 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
   comments.push({ id: commentId, content });
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post('http://localhost:5000/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
+
   res.status(201).send(comments);
 });
 
