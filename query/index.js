0fc5c69d35142faa365beb8fcb0bd02b694ee616
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-12-28 04:00:44 +0100

    feat: parsing incoming events

--- query/index.js
+++ query/index.js
@@ -5,12 +5,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const posts = {};
+
 app.post('/events', (req, res) => {
   const { type, data } = req.body;
+
+  switch (type) {
+    case 'PostCreated':
+      const { id, title } = data;
+      posts[id] = { id, title, comments: [] };
+      break;
+    case 'CommentCreated':
+      const { id, content, postId } = data;
+      const post = posts[postId];
+      post.comments.push({
+        id,
+        content,
+      });
+      break;
+    default:
+      break;
+  }
   res.send({});
 });
 
-app.get('/posts', (req, res) => {});
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
 
 app.listen(4002, () => {
   console.log('Listening on 4002');
