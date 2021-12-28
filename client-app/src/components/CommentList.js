5f3656cf30a453d4b5aec2b5ec2faaef155f0284
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-12-28 15:39:54 +0100

    feat: using query service in client

--- client-app/src/components/CommentList.js
+++ client-app/src/components/CommentList.js
@@ -1,23 +1,6 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-
-export const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchComments = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-    setComments(res.data);
-  };
-
-  useEffect(() => {
-    fetchComments();
-  }, []);
-
+export const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => (
     <li key={comment.id}>{comment.content}</li>
   ));
-
   return <ul>{renderedComments}</ul>;
 };
--- client-app/src/components/PostList.js
+++ client-app/src/components/PostList.js
@@ -6,7 +6,7 @@ export const PostList = () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get('http://localhost:4000/posts');
+    const res = await axios.get('http://localhost:4002/posts');
     setPosts(res.data);
   };
 
@@ -23,7 +23,7 @@ export const PostList = () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
+          <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
