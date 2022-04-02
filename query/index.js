bcfc697c00fbffd47848d349066590dd9a1284d4
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2022-04-02 13:40:54 +0100

    refactor: update query url service

--- query/index.js
+++ query/index.js
@@ -46,7 +46,7 @@ app.get('/posts', (req, res) => {
 
 app.listen(4002, async () => {
   console.log('Listening on 4002');
-  const res = await axios.get('http://localhost:4005/events');
+  const res = await axios.get('http://event-bus-srv:4005/events');
 
   for (const event of res.data) {
     const { type, data } = event;
