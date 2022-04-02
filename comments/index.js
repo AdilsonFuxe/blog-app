d32ca8578bb840b57ef84c6acbc63b9cd76ae4e3
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2022-04-02 13:38:08 +0100

    refactor: update comments url service

--- comments/index.js
+++ comments/index.js
@@ -21,7 +21,7 @@ app.post('/posts/:id/comments', async (req, res) => {
   comments.push({ id: commentId, content, status });
   commentsByPostId[req.params.id] = comments;
 
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://event-bus-srv:4005/events', {
     type: 'CommentCreated',
     data: {
       id: commentId,
@@ -45,7 +45,7 @@ app.post('/events', async (req, res) => {
     const comment = comments.find((comment) => comment.id === id);
     comment.status = status;
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
       type: 'CommentUpdated',
       data: {
         postId,
